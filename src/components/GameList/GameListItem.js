58758d6b7fd3dd34f7c53d0339d7cb7cdf79ad9b
SpikeHD <spikegdofficial@gmail.com>
2020-10-31 18:43:32 -0700

    Images and RAWG api

--- src/components/GameList/GameListItem.js
+++ src/components/GameList/GameListItem.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Rawger from 'rawger'
 
 class GameListItem extends React.Component {
   constructor(props) {
@@ -6,11 +7,22 @@ class GameListItem extends React.Component {
 
     this.name = props.name
     this.link = props.link
+
+    this.state = {image: 'https://via.placeholder.com/150'}
+  }
+
+  getRawgImage = async () => {
+    const {games} = await Rawger()
+    const results = await games.search(this.name)
+    const first = results.findOne()
+
+    this.setState({image: first.image})
   }
 
   render() {
     return (
-      <div className="gameListItem">
+      <div className="gameListItem" link={this.link} onClick={this.getRawgImage}>
+        <img src={this.state.image} alt="Game Image" height="150" width="150"/>
         <p className="title">{this.name}</p>
       </div>
     )
