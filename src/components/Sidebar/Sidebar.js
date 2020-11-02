7e55a06834bb66a8502590d814358a4ebf815012
spikegd <spikegdofficial@gmail.com>
2020-11-01 21:54:43 -0800

    Sidebar

---
+++ src/components/Sidebar/Sidebar.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { slide as Menu } from 'react-burger-menu'
+import './Sidebar.css'
+
+class Sidebar extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return(
+      <Menu>
+        <a href="/">Game List</a>
+        <a href="/settings">Settings</a>
+      </Menu>
+    )
+  }
+}
+
+export default Sidebar
