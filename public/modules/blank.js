0d4a85c3b5f491d877d8088ce37359fdb2a0e237
SpikeHD <spikegdofficial@gmail.com>
2020-11-08 02:22:16 -0800

    Add blank module for first time setup

---
+++ public/modules/blank.js
@@ -0,0 +1,22 @@
+/**
+ * Empty meta info
+ */
+module.exports.meta = {
+  name: 'None',
+  description: 'No selected module',
+  cachePath: 'blank.json'
+}
+
+/**
+ * Blank array
+ */
+module.exports.getAllGames = () => {
+  return []
+}
+
+/**
+ * Blank object
+ */
+module.exports.getGame = () => {
+  return {}
+}
